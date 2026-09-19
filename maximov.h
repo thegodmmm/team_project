6abb691559695ed3b58d2b27c269ac2ddf7576e9
thegodmmm <maksi.26.02.07@gmail.com>
2026-09-19 18:46:27 +0700

    Перенес функции варианта 38

---
+++ maximov.h
@@ -0,0 +1,6 @@
+#ifndef	MAXIMOV_H
+#define	MAXIMOV_H
+	//	Функции	варианта	38	(Максимов	М.	М.)
+double	currentWork(double	U, double	I, double	t);
+double	jouleHeat(double	I, double	R, double t);
+#endif	//	MAXIMOV_H
